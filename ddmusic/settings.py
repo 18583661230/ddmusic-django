6b6874aab5e321da503361b9f6628654c1118429
zhengjian8086 <65802295+zhengjian8086@users.noreply.github.com>
2020-07-20 21:36:27 +0800

    Update DB&setting for my env

--- ddmusic/settings.py
+++ ddmusic/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ddmusic',
-        'USER': 'root',
-        'PASSWORD': '123456',
+        'USER': 'guest_q',
+        'PASSWORD': '123',
         'HOST': '127.0.0.1',
         'PORT': 3306
     }
